46656894beffe0a8180045281edc7ac74b7e7154
Rafeh Qazi <qazi@cleverprogrammer.com>
2021-10-06 18:51:35 -0700

    ✅ ADD: Chessboard that can make valid moves

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Chessboard from 'chessboardjsx'
+import * as Chess from 'chess.js'
+
+const STARTING_POSITION =
+  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
 function App() {
+  const [position, setPosition] = useState(STARTING_POSITION)
+
+  const [chessBoard] = useState(new Chess())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h2>Qazi's chess... 👇</h2>
+        <Chessboard
+          position={position}
+          onDrop={({ sourceSquare, targetSquare }) => {
+            chessBoard.move({ from: sourceSquare, to: targetSquare })
+            setPosition(chessBoard.fen())
+          }}
+        />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
