5275277ea0182e76f8ce9ca9d2a2f1acf913f058
Saarthak-Slathia <saarthak.slathia@gmail.com>
2021-10-07 09:06:17 +0530

    Add Reset Game Functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import Chessboard from 'chessboardjsx'
-import * as Chess from 'chess.js'
+import {useState} from "react";
+import "./App.css";
+import Chessboard from "chessboardjsx";
+import * as Chess from "chess.js";
 
 const STARTING_POSITION =
-  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 function App() {
-  const [position, setPosition] = useState(STARTING_POSITION)
+  const [position, setPosition] = useState(STARTING_POSITION);
 
-  const [chessBoard] = useState(new Chess())
+  const [chessBoard, setChessBoard] = useState(new Chess());
+
+  // Function to reset the game
+  const resetGame = async () => {
+    setPosition(STARTING_POSITION);
+    setChessBoard(new Chess());
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <h2>Qazi's chess... 👇</h2>
         <Chessboard
           position={position}
-          onDrop={({ sourceSquare, targetSquare }) => {
-            chessBoard.move({ from: sourceSquare, to: targetSquare })
-            setPosition(chessBoard.fen())
+          onDrop={({sourceSquare, targetSquare}) => {
+            chessBoard.move({from: sourceSquare, to: targetSquare});
+            setPosition(chessBoard.fen());
           }}
         />
+        <button
+          style={{
+            margin: "0.5rem",
+            padding: "7px 10px",
+            backgroundColor: "white",
+            border: "2px solid white",
+            cursor: "pointer",
+          }}
+          id="start"
+          onClick={resetGame}
+        >
+          New Game
+        </button>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
